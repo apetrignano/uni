1a33499dc0b0ad45acefdd6c84aa4c8c70782c18
adriano <angelo.petrignano@gmail.com>
2026-05-13 14:27:20 +0200

    fixed 7-commenti-range_tempo.js

--- programmazione_web/labo/compiti_lezione_13/7-commenti-range_tempo.js
+++ programmazione_web/labo/compiti_lezione_13/7-commenti-range_tempo.js
@@ -23,19 +23,14 @@ app.get('/comments', async(req, res) => {
 
   const max = new Date(req.query.max);
   const min = new Date(req.query.min);
-  const email = req.query.email;
 
   if(!max||!min) {
     return res.status(400).json({errore: 'Specificare il range di date in cui ricercare!'});
-  }
-  if(!email) {
-    return res.status(400).json({errore: 'Bisogna inserire una email!'});
   }
 
   const collection = db.collection('comments');
 
   const filtro = {
-    email: email,
     date: {
       $gte: min,
       $lte: max
@@ -44,7 +39,7 @@ app.get('/comments', async(req, res) => {
   const films = await collection
     .find(filtro)
     .limit(30)
-    .project({ movie_id: 1, text:1, _id: 0 })
+    .project({ movie_id: 1, text:1, date:1, _id: 0 })
     .toArray();
 
   res.status(200).json(films);
@@ -57,7 +52,7 @@ connectDB()
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server avviato su http://localhost:${PORT}`);
-      console.log(`Link di esempio: http://localhost:${PORT}/comments?email=mercedes_tyler@fakegmail.com&min=2002-08-18&max=2015-12-10`)
+      console.log(`Link di esempio: http://localhost:${PORT}/comments?min=2002-08-18&max=2015-12-10`)
     });
   })
   .catch(err => {
